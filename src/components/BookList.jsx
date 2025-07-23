11f3124eb7302a058ea6370a28e346724cb4bcd1
Rufat1902 <valiyev.rufat96@gmail.com>
2025-07-23 16:52:29 +0200

    Adds Search input

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,12 +1,28 @@
-import SingleBook from './SingleBook';
+import SingleBook from "./SingleBook";
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 const BookList = ({ books }) => {
+  const [selectedword, setSelectedword] = useState("");
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(selectedword.toLowerCase())
+  );
   return (
-    <div className="d-flex flex-wrap gap-3 justify-content-center">
-      {books.map((book) => (
-        <SingleBook key={book.asin} book={book} />
-      ))}
-    </div>
+    <>
+      <Form className="mb-3" style={{ width: "200px", marginRight: "auto", marginLeft: "auto" }}>
+        <Form.Control
+          type="text"
+          placeholder="Search book..."
+          value={selectedword}
+          onChange={(e) => setSelectedword(e.target.value)}
+        />
+      </Form>
+      <div className="d-flex flex-wrap justify-content-center">
+        {filteredBooks.map((book) => (
+          <SingleBook key={book.asin} book={book} />
+        ))}
+      </div>
+    </>
   );
 };
 
