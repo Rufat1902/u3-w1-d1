4227687b6c2d629adbdc4c7e15136d627381f86d
Rufat1902 <valiyev.rufat96@gmail.com>
2025-07-23 16:22:38 +0200

    Adds BookList

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import NavbarComponent from "./components/NavbarComponent";
 import MyFooter from "./components/MyFooter";
 import Welcome from "./components/Welcome";
 // import AllTheBooks from './components/AllBooks';
-import SingleBook from "./components/SingleBook";
-import fantasy from "./data/fantasy.json"; // Assuming you have a fantasy.json file in the data folder
+// import SingleBook from "./components/SingleBook";
+import fantasy from "./data/fantasy.json"; 
+import BookList from "./components/BookList"; 
 
 function App() {
   return (
     <>
       <NavbarComponent tema="dark" />
       <Welcome />
+      <BookList books={fantasy} />
       {/* <AllTheBooks /> */}
-      <SingleBook book={fantasy[0]} />
+      {/* <SingleBook book={fantasy[0]} /> */}
       <MyFooter />
     </>
   );
